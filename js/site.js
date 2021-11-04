cd00f1c37cdd1da726716bdde83a27c61d19d1fe
StaticShock93 <74117562+StaticShock93@users.noreply.github.com>
2021-11-04 11:23:36 -0700

    updated js and code.html

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 // get button, add event listener onclick
+// ------------------generate values functions----------------
 const btn = document.getElementById("btnSubmit");
 btn.addEventListener("click", runShit);
 function runShit() {
@@ -22,38 +23,16 @@ function runShit() {
     return pbTemplate;
   }
 
-  // function monthlyInterest(loan, term, rate) {
-  //   let interest = loan * (rate / 100 / 365) * 30;
-  //   interest *= term;
-  //   interest = parseFloat(interest);
-  //   return interest.toFixed(2);
-  // }
-
   function totalMonthlyPayment(loan, term, rate) {
     let totalMonthlyPayment =
       (loan * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
     return totalMonthlyPayment.toFixed(2);
   }
 
-  // function totalInterest(interestArray) {
-  //   interestArray.reduce((total, el) => {
-  //     return total + el;
-  //   });
-  // }
-
-  // function totalInterest(loan, rate, term) {
-  //   let totalInterest = loan * (rate / 100);
-  //  totalInterest = parseFloat(totalInterest)
-  //   return totalInterest.toFixed(2);
-  // }
-
   function totalCost(loan, totalInterest) {
     totalInterest = parseFloat(totalInterest);
     loan = parseFloat(loan);
-    // console.log(loan);
-    // console.log(totalInterest);
     loan += totalInterest;
-    // console.log(loan);
     return loan.toFixed(2);
   }
 
@@ -68,7 +47,10 @@ function runShit() {
   function paymentsArray(totalMonthlyPayment, term) {
     const paymentsArray = [];
     for (let i = 0; i <= term; i++) {
-      paymentsArray.push(totalMonthlyPayment);
+      paymentsArray.push(
+        "$" +
+          totalMonthlyPayment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
     }
     return paymentsArray;
   }
@@ -81,7 +63,13 @@ function runShit() {
       monthlyInterest /= 12;
       let principalAmount = totalMonthlyPament - monthlyInterest;
       remainingBalance -= principalAmount;
-      remainingBalanceArray.push(remainingBalance.toFixed(2));
+      remainingBalanceArray.push(
+        "$" +
+          remainingBalance
+            .toFixed(2)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
     }
     return remainingBalanceArray;
   }
@@ -95,14 +83,18 @@ function runShit() {
       monthlyInterest /= 12;
       let principalAmount = totalMonthlyPayment - monthlyInterest;
       remainingBalance -= principalAmount;
-      interestArray.push(monthlyInterest.toFixed(2));
+      interestArray.push(
+        "$" +
+          monthlyInterest
+            .toFixed(2)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
     }
-    // console.log(interestArray);
     return interestArray;
   }
- 
 
-  function totalInterest(loan, totalMonthlyPayment, term, rate){
+  function totalInterest(loan, totalMonthlyPayment, term, rate) {
     const interestArray = [];
     let remainingBalance = loan;
     console.log(term);
@@ -113,9 +105,8 @@ function runShit() {
       remainingBalance -= principalAmount;
       interestArray.push(monthlyInterest);
     }
-    // console.log(interestArray);
-    let sumOfInterest = interestArray.reduce((a,b)=> a+b)
-    console.log(sumOfInterest)
+    let sumOfInterest = interestArray.reduce((a, b) => a + b);
+    console.log(sumOfInterest);
     return sumOfInterest.toFixed(2);
   }
 
@@ -127,16 +118,19 @@ function runShit() {
       monthlyInterest /= 12;
       let principalAmount = totalMonthlyPament - monthlyInterest;
       remainingBalance -= principalAmount;
-      principalArray.push(principalAmount.toFixed(2));   
-     }
-     console.log(principalArray)
+      principalAmount = principalAmount.toFixed(2);
+      principalArray.push(
+        "$" + principalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
+    }
+    console.log(principalArray);
     return principalArray;
   }
 
-  function totalMonthlyInterest(loan,rate,term,totalMonthlyPayment) {
+  function totalMonthlyInterest(loan, rate, term, totalMonthlyPayment) {
     const interestArray = [];
-    const monthlyInterestArray = []
-    let monthlyTotal = 0
+    const monthlyInterestArray = [];
+    let monthlyTotal = 0;
     let remainingBalance = loan;
     console.log(term);
     for (let i = 0; i <= term; i++) {
@@ -146,12 +140,19 @@ function runShit() {
       remainingBalance -= principalAmount;
       interestArray.push(monthlyInterest);
     }
-    interestArray.forEach(el => {
-       monthlyTotal += el
-      monthlyInterestArray.push(monthlyTotal.toFixed(2))
-    })
-    return monthlyInterestArray
-    }
+    interestArray.forEach((el) => {
+      monthlyTotal += el;
+      monthlyTotal = monthlyTotal;
+      monthlyInterestArray.push(
+        "$" +
+          monthlyTotal
+            .toFixed(2)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
+    });
+    return monthlyInterestArray;
+  }
 
   //------------------------dipslay functions------------------------
   // ----display totals section
@@ -182,33 +183,22 @@ function runShit() {
     tableBody.innerHTML = "";
 
     for (let index = 0; index < term; index += 1) {
-      // let monthArr = [];
-      // for (let i = 0; i < term; i++) {
-      //   monthArr.push(monthArr[i]);
-      // }
-      // let month = monthArr;
+    
 
       let tableRow = document.importNode(templateRow.content, true);
 
-      let rows = tableRow.querySelectorAll("tr");
       let rowCols = tableRow.querySelectorAll("td");
 
-      // rowCols[0].classList.add(months[index]);
       rowCols[0].textContent = months[index];
 
-      // rowCols[1].classList.add(months[index]);
       rowCols[1].textContent = paymentsArray[index];
 
-      // rowCols[2].classList.add(months[index]);
       rowCols[2].textContent = principalArray[index];
 
-      // rowCols[3].classList.add(months[index]);
       rowCols[3].textContent = interestArray[index];
 
-      // rowCols[4].classList.add(months[index]);
       rowCols[4].textContent = monthlyInterestTotal[index];
 
-      // rowCols[5].classList.add(months[index]);
       rowCols[5].textContent = remainingBalanceArray[index];
 
       // add all the rows to the table
@@ -229,13 +219,15 @@ function runShit() {
     term1,
     rate1
   );
-  console.log(typeof monthlyInterest1.reduce((a,b)=> a + b))
+  console.log(typeof monthlyInterest1.reduce((a, b) => a + b));
 
-  const totalInterest1 = totalInterest( loan1,
+  const totalInterest1 = totalInterest(
+    loan1,
     totalMonthlyPayment1,
     term1,
-    rate1);
-  console.log(totalInterest1)
+    rate1
+  );
+  console.log(totalInterest1);
   const totalCost1 = totalCost(loan1, totalInterest1);
   const months1 = months(term1);
   const payments1 = paymentsArray(totalMonthlyPayment1, term1);
@@ -250,13 +242,13 @@ function runShit() {
     rate1,
     term1,
     totalMonthlyPayment1
-  )
+  );
   const monthlyInterestTotal1 = totalMonthlyInterest(
     loan1,
     rate1,
     term1,
     totalMonthlyPayment1
-  )
+  );
   const displayTable1 = displayTable(
     term1,
     months1,
@@ -265,7 +257,6 @@ function runShit() {
     monthlyInterest1,
     principalArray1,
     monthlyInterestTotal1
-    // interestArray here
   );
   const displayTotals1 = displayTotals(
     totalMonthlyPayment1,
@@ -274,44 +265,3 @@ function runShit() {
     loan1
   );
 }
-
-// add event listener and run functionality
-
-// btn.addEventListener("click", loan1);
-// btn.addEventListener("click", term1);
-// btn.addEventListener("click", rate1);
-// btn.addEventListener("click", pbTemplate1);
-// btn.addEventListener("click", totalMonthlyPayment1);
-// btn.addEventListener("click", totalInterest1);
-// btn.addEventListener("click", totalCost1);
-// btn.addEventListener("click", displayTotals1)
-// btn.addEventListener('click', () => {
-//     let loan = document.getElementById("loanAmount").value;
-//     let term = document.getElementById("term").value;
-//     let rate = document.getElementById("rate").value;
-//     console.log(loan)
-
-//     let totalMonthlyPayment = (loan * (rate / 1200)) / (1 - (1 + rate / 1200) ** -term);
-//     let totalPrincipal = loan;
-//     let totalInterest = (loan * rate) / 1200;
-//     let totalCost = totalPrincipal + totalInterest;
-//     console.log(totalPrincipal)
-
-//     document.getElementById('totalMonthlyPayments').innerHTML = "$" + totalMonthlyPayment.toFixed(2)
-
-//     // document.getElementById('totalPrincipal').innerHTML = '$' + totalPrincipal.toFixed(2)
-//     // document.getElementById('totalInterest').innerHTML = '$' + totalInterest.toFixed(2)
-//     // document.getElementById('totalCost').innerHTML = '$' + totalCost.toFixed(2)
-// })
-// btn.addEventListener('click', () => {
-//     let loan = document.getElementById("loanAmount").value;
-//     document.getElementById('totalPrincipal').innerHTML = "$" + totalInterest.toFixed(2)
-
-// })
-
-//
-
-// use retrieved values to create needed generated values
-
-// display the generated values to the page in the imported template and specified locations
-// // display function should display all numbers at once
